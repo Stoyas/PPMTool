e2be14e1a34fcf362cd6cd0370e1301ef09a7102
Kyle Zhao <kyle.zhao2011@gmail.com>
2020-02-11 00:45:23 -0500

    initilize

---
+++ src/main/java/kylegroup/ppmtoolfullstack/PpmtoolfullstackApplication.java
@@ -0,0 +1,13 @@
+package kylegroup.ppmtoolfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolfullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolfullstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kylegroup/ppmtoolfullstack/PpmtoolfullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package kylegroup.ppmtoolfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolfullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
